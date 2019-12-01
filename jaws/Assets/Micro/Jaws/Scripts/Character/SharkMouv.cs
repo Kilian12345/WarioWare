e19a09bc045c01ca422eb7c64d9514331da9623e
Kilian <allairekilian@gmail.com>
2019-12-01 22:28:05 +0100

    jump 30%

--- jaws/Assets/Micro/Jaws/Scripts/Character/SharkMouv.cs
+++ jaws/Assets/Micro/Jaws/Scripts/Character/SharkMouv.cs
@@ -10,6 +10,13 @@ namespace Game.Jaws
         public float sharkSpeed = 0.01f;
         float mouseBasePosition;
 
+        [Space(10)]
+        [Header("Curve")]
+        public float curveHeight;
+        public float curveWidth;
+
+
+
         private void Start()
         {
             mouseBasePosition = 0;
@@ -17,11 +24,28 @@ namespace Game.Jaws
 
         private void Update()
         {
+            Mouvement();
+
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+
+        void Mouvement()
+        {
             mouseBasePosition += Input.GetAxis("Mouse X") * sharkSpeed;
 
             Vector3 pos = transform.position;
             pos.x += mouseBasePosition;
             pos.x = Mathf.Clamp(pos.x, PointA.position.x, PointB.position.x);
+            transform.position = pos;
+        }
+
+        void Jump()
+        {
+            Vector3 pos = transform.position;
+            pos.y = Mathf.Sin(pos.x * curveWidth);
             transform.position = pos;
         }
     }
