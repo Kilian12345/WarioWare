6835da60fa8ae507ca60a4e6a816cf743485302c
Kilian <allairekilian@gmail.com>
2020-01-12 17:47:55 +0100

    Boué

--- jaws/Assets/Micro/Jaws/Scripts/Manager/GameManager.cs
+++ jaws/Assets/Micro/Jaws/Scripts/Manager/GameManager.cs
@@ -6,17 +6,18 @@ namespace Game.Jaws
     {
         [SerializeField] GameObject Obstacle1;
         [SerializeField] GameObject Obstacle2;
+        [SerializeField] GameObject Obstacle3;
 
         private void Start()
         {
             if(Macro.Difficulty == 2)
             {
-                Instantiate(Obstacle1, new Vector3(Random.Range(-5.4f, 5.4f), -0.9f, 0), Quaternion.identity, transform.parent);
+                Instantiate(Obstacle1, new Vector3(Random.Range(-5.4f, 5.4f), -0.9f, 0), Quaternion.identity, transform);
             }
             else if (Macro.Difficulty == 3)
             {
-                Instantiate(Obstacle1, new Vector3(Random.Range(-5.4f, 0), -0.9f, 0), Quaternion.identity, transform.parent);
-                Instantiate(Obstacle2, new Vector3(Random.Range(0, 5.4f), -0.9f, 0), Quaternion.identity, transform.parent);
+                Instantiate(Obstacle3, new Vector3(Random.Range(-5.4f, 0), -0.9f, 0), Quaternion.identity, transform);
+                Instantiate(Obstacle2, new Vector3(Random.Range(0, 5.4f), -0.9f, 0), Quaternion.identity, transform);
             }
 
         }   
